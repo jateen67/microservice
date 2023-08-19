ea41a7b9d2e6ce90faf89a98a74ce1454fbe9b20
exit <kalsijatin67@gmail.com>
2023-08-19 04:01:02 -0400

    added sent and received console info

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,16 @@ import "./App.css";
 
 function App() {
   const getRoot = async () => {
+    const body = {
+      Content: "Empty request",
+    };
+
     const res = await fetch("http://localhost:80", {
       method: "POST",
+      body: JSON.stringify(body),
     });
+
+    console.log(body);
     console.log(await res.json());
   };
 
