7efc773f84113db66b55e36b457bbcd26c368928
exit <kalsijatin67@gmail.com>
2023-08-29 06:17:04 -0400

    made rabbitmq docker work

--- listener/main.go
+++ listener/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("ack")
+
+	conn, err := connectToRabbitMQ()
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer conn.Close()
+
+	log.Println("successfully connected to rabbitmq")
+}
+
+func connectToRabbitMQ() (*amqp.Connection, error) {
+	var count int64
+	var connection *amqp.Connection
+
+	for {
+		conn, err := amqp.Dial(os.Getenv("RABBITMQ_CONNECTION_STRING"))
+		if err != nil {
+			log.Println("rabbitmq not yet ready...")
+			count++
+		} else {
+			connection = conn
+			break
+		}
+
+		if count > 10 {
+			log.Println(err)
+			return nil, err
+		}
+
+		log.Println("retrying in 2 seconds...")
+		time.Sleep(2 * time.Second)
+	}
+
+	return connection, nil
 }
